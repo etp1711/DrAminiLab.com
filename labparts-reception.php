f37da197081ceed9a9790d42a616b8f519240016
etp1711 <etp1711@gmail.com>
2014-07-27 13:37:25 +0430

    add orginal files

--- labparts-reception.php
+++ labparts-reception.php
@@ -1,12 +1,11 @@
 ﻿<?php include('./inc/header.php') ?>
 	
-	<section class="middle">
+	<section class="middle-labparts-reception">
 		<div class="labparts-reception">
 			<div class="contain">
 				<h1>پذیرش</h1>
-				<img  src="images/parts/3.jpg"/>
-
-
+				<img  src="images/parts/1.jpg"/>
+				
 			</div>
 		</div>
 	</section>
