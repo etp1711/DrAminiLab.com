db56dcd0892bb0126851404d2a625bb8346e627c
etp1711 <etp1711@gmail.com>
2015-09-03 19:13:09 +0430

    dasd

--- newtest.php
+++ newtest.php
@@ -4,7 +4,7 @@
 		<div class="newtest">
 			
 
-		<p>		BMP	<br /><br />AMH<br /><br />IGFBP3<br /><br />ANCA<br /><br />C-peptide<br /><br />inhibin A<br /><br />HE4<br /><br />DHT			</p>	
+		<p>	BMP	<br /><br />AMH<br /><br />IGFBP3<br /><br />ANCA<br /><br />C-peptide<br /><br />inhibin A<br /><br />HE4<br /><br />DHT			</p>	
 
 		</div>
 	</section>
---
+++ response--2 - Copy.php
@@ -0,0 +1,14 @@
+﻿
+<?php include('./inc/header.php') ?>
+
+	<section class="bigresponse">
+		<div class="response">
+
+
+			<iframe src="http://www.sabapouyan.com/RespLogin.aspx?mid=11" allowtransparency="true" scrolling="no"  />
+
+			</iframe>
+		</div>
+	</section>	
+	
+<?php include('./inc/footer.php') ?>
