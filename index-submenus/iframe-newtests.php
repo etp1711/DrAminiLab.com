b28e6fea26049d06d221c05b5dd98911debaeb96
etp1711 <etp1711@gmail.com>
2015-10-11 13:08:49 +0330

    asd

---
+++ index-submenus/iframe-newtests.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="iframestyle.css"/>
+</head>
+
+
+
+<body>
+		
+
+		<section class="middle">
+			<div class="newtest">
+			
+
+				<p>	BMP	<br /><br />AMH<br /><br />IGFBP3<br /><br />ANCA<br /><br />C-peptide<br /><br />inhibin A<br /><br />HE4<br /><br />DHT			</p>	
+
+			</div>
+		</section>
+
+
+</body>
+</html>
