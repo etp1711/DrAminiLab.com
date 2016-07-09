1f9a3fb1d9a490712cc01b6c4e81f5602c929cb4
etp1711 <etp1711@gmail.com>
2016-07-09 11:00:33 +0430

    asD

--- guide.php
+++ guide.php
@@ -1,29 +1,120 @@
-﻿<?php include('./inc/rotate/uphead.php') ?>
-	<!-- For Rotate -->
-	<style type="text/css">
-	section.middle .about{
-     -webkit-transition:	all 0ms;}
-	body.preload section.middle .about{
-        -webkit-transform:rotate(0deg) scale(0.2,0.2) translateY(0px) translateX(-5000px);}
-</style><script type="text/javascript">window.onload=function(){document.body.className = '';}</script>
-<!-- End Of Rotate -->
-<?php include('./inc/rotate/downhead.php') ?>
+﻿<?php include('./inc/header.php') ?>
 
 
 
 	<section class="middle">
 		<div class="guide">
-			
+
+
+			<div class="menubar">
+				<div class="menu">
+					<ul>
+						<li><a href="employment.php">راهنمای بیماران</a></li>	
+						<li><a href="contact.php">آزمایشگاه های همکار</a></li>	
+					</ul>	
+				</div>	
+			</div>
 
 
 
+			<div class=otherlabs>
+				
+						<!-- 3 BOXXESS  -->
+				<div class="threecircle"><h2>آزمایشگاه های محترم همکار</h2>
+					<div class="content">
+
+
+
+						<!-- LEFT -->
+						<div class="left">
+							<h1>پذیرش ارسالی</h1>
+							<p>به زودی در این قسمت امکان ثبت پذیرش تستها به صورت آنلاین قرار داده میشود</p>
+													
+						</div>
+
+
+
+						<!-- Middle -->
+						<div class="middle">
+							<h1>اطلاعات تست ها</h1>
+						</div>
+
+
+
+
+						<!-- Right -->
+						<div class="right">
+							<h1>دانلود فرمها </h1><br />
+							<a  href="index-downloads/gharbalgari.pdf"><span>دانلود فرم غربالگری</span></a> <br /><br /><br />
+							<a  href="index-downloads/ersali.pdf"><span>دانلود فرم ارسالی</span></a> <br /><br /><br />
+							<a  href="index-downloads/PriceList95.xls"><span>دانلود قیمتهای آزاد و دولتی 95</span></a>
+							
+						</div>
+
+
+
+
+
+						<div class="badboy"></div>
+					</div>
+				</div>
+			</div>
+
+
+
+
+
+			<!-- Rahnamae Bimaran -->
+
+			<div class=patient>
 				
 
 
+					<!-- 3 BOXXESS   -->
+				<div class="threecircle"><h2>راهنمای بیماران</h2>
+					<div class="content">
 
 
 
+						<!-- LEFT -->
+						<div class="left">
+							<h1>نمونه گيري در منزل</h1>
+							<p>بيماران عزيزي که به هر دليلي نميتوانند به آزمايشگاه مراجعه نمياند ميتوانند از سرويس نمونه گيري منزل استفاده کنند.
+							 براي اين امر ميتوانيد روز قبل با مسول مربوطه آقاي عرفانيان همانگ کرده و يا با شماره 38434119 به صورت تلفني هماهنگ نماييد.</p>
+													
+						</div>
+
+
+
+						<!-- Middle -->
+						<div class="middle">
+							<h1>سوالات متدوال</h1>
+						</div>
+
+
+
+
+						<!-- Right -->
+						<div class="right">
+							<h1>راهنماي آزمايش</h1>
+							
+							
+						</div>
+
+
+
+
+
+						<div class="badboy"></div>
+					</div>
+				</div>
 			</div>
+		</div>
+
+
+
+
+			
 	</section>
 		
 <?php include('./inc/footer.php') ?>
